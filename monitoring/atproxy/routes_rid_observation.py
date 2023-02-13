c3581596c28a35cdd68a5688d427dddbdbb34024
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2023-02-13 08:08:51 -0800

    [atproxy] Improve atproxy logging and reduce atproxy_client daemon failures (#38)
    * Improve atproxy logging and reduce atproxy_client daemon failures
    
    * Remove non-serializable MappingProxyType

--- monitoring/atproxy/routes_rid_observation.py
+++ monitoring/atproxy/routes_rid_observation.py
@@ -1,4 +1,3 @@
-import logging
 from typing import Tuple
 
 import flask
@@ -10,20 +9,15 @@ from .oauth import requires_scope
 from .requests import RIDObservationGetDisplayDataRequest, RIDObservationGetDetailsRequest
 
 
-logging.basicConfig()
-_logger = logging.getLogger('atproxy.rid_observation')
-_logger.setLevel(logging.DEBUG)
-
-
 @webapp.route('/riddp/observation/display_data', methods=['GET'])
 @requires_scope([Scope.Read])
 def rid_observation_display_data() -> Tuple[str, int]:
     """Implements retrieval of current display data per automated testing API."""
-    return handling.fulfill_query(RIDObservationGetDisplayDataRequest(view=flask.request.args['view']), _logger)
+    return handling.fulfill_query(RIDObservationGetDisplayDataRequest(view=flask.request.args['view']))
 
 
 @webapp.route('/riddp/observation/display_data/<flight_id>', methods=['GET'])
 @requires_scope([Scope.Read])
 def rid_observation_flight_details(flight_id: str) -> Tuple[str, int]:
     """Implements get flight details endpoint per automated testing API."""
-    return handling.fulfill_query(RIDObservationGetDetailsRequest(id=flight_id), _logger)
+    return handling.fulfill_query(RIDObservationGetDetailsRequest(id=flight_id))
