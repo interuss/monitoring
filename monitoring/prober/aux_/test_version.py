c89aa25c1f2118443b96654420a14f9f0daa0b66
Michael Barroco <michael@orbitalize.com>
2023-03-22 11:57:22 -0400

    format

--- monitoring/prober/aux_/test_version.py
+++ monitoring/prober/aux_/test_version.py
@@ -5,8 +5,8 @@ from uas_standards.astm.f3411 import v19
 
 
 def test_version(aux_session):
-  resp = aux_session.get('/version', scope=v19.constants.Scope.Read)
-  assert resp.status_code == 200
-  version = resp.json()['version']
-  assert version
-  assert 'undefined' not in version, version
+    resp = aux_session.get("/version", scope=v19.constants.Scope.Read)
+    assert resp.status_code == 200
+    version = resp.json()["version"]
+    assert version
+    assert "undefined" not in version, version
