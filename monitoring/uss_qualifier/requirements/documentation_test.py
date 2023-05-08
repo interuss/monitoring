66dbe2f0ebbeda2b2b53f5b545d7b0e8a51c90b8
Michael Barroco <michael@orbitalize.com>
2023-05-08 21:34:04 +0200

    format

--- monitoring/uss_qualifier/requirements/documentation_test.py
+++ monitoring/uss_qualifier/requirements/documentation_test.py
@@ -3,15 +3,11 @@ from .definitions import RequirementCollection, RequirementSetID
 
 
 def test_requirements_extraction():
-    collection = RequirementSetID(
-        "interuss.uss_qualifier.unit_test.set1"
-    )
+    collection = RequirementSetID("interuss.uss_qualifier.unit_test.set1")
     requirement_set = get_requirement_set(collection)
     assert len(requirement_set.requirement_ids) == 3
 
-    collection = RequirementSetID(
-        "interuss.uss_qualifier.unit_test.set1#Manual Checks"
-    )
+    collection = RequirementSetID("interuss.uss_qualifier.unit_test.set1#Manual Checks")
     requirement_set = get_requirement_set(collection)
     assert len(requirement_set.requirement_ids) == 1
 
