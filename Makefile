1816a62641f2ce410793781640f90178daf610a5
Michael Barroco <michael@orbitalize.com>
2023-07-07 18:06:17 +0200

    Switch tracer to v22a and remove v19 rid mocks

--- Makefile
+++ Makefile
@@ -73,12 +73,10 @@ collect-local-logs:
 	-sh -c "build/dev/run_locally.sh logs --timestamps" > logs/dss_sandbox_local.log 2>&1
 	-docker logs atproxy > logs/atproxy.log 2>&1
 	-docker logs mock_uss_scdsc > logs/mock_uss_scdsc.log 2>&1
-	-docker logs mock_uss_ridsp > logs/mock_uss_ridsp.log 2>&1
 	-docker logs mock_uss_ridsp_v22a > logs/mock_uss_ridsp_v22a.log 2>&1
-	-docker logs mock_uss_riddp > logs/mock_uss_riddp.log 2>&1
 	-docker logs mock_uss_riddp_v22a > logs/mock_uss_riddp_v22a.log 2>&1
 	-docker logs mock_uss_geoawareness > logs/mock_uss_geoawareness.log 2>&1
-	-docker logs mock_uss_tracer > logs/mock_uss_tracer.log 2>&1
+	-docker logs mock_uss_tracer_v22a > logs/mock_uss_tracer_v22a.log 2>&1
 	-docker logs mock_uss_atproxy_client > logs/mock_uss_atproxy_client.log 2>&1
 
 .PHONY: stop-locally
--- monitoring/mock_uss/start_all_local_mocks.sh
+++ monitoring/mock_uss/start_all_local_mocks.sh
@@ -14,17 +14,14 @@ cd "${BASEDIR}/../.." || exit 1
 
 monitoring/mock_uss/run_locally_scdsc.sh -d
 export DO_NOT_BUILD_MONITORING=true
-monitoring/mock_uss/run_locally_ridsp.sh -d
 monitoring/mock_uss/run_locally_ridsp_v22a.sh -d
-monitoring/mock_uss/run_locally_riddp.sh -d
 monitoring/mock_uss/run_locally_riddp_v22a.sh -d
 monitoring/mock_uss/run_locally_geoawareness.sh -d
 monitoring/mock_uss/run_locally_atproxy_client.sh -d
 monitoring/mock_uss/run_locally_tracer.sh -d
+monitoring/mock_uss/run_locally_tracer_v22a.sh -d
 monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_scdsc
-monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_ridsp
 monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_ridsp_v22a
-monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_riddp
 monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_riddp_v22a
 monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_geoawareness
-monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_tracer
+monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_tracer_v22a
