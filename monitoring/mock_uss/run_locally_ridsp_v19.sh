e4828d6f29b92a47e831d14018716f3ee9142f2d
Michael Barroco <michael@orbitalize.com>
2023-07-12 14:06:29 +0200

    [uss_qualifier] Switch default ASTM NETRID to v22a and reorganize CI tests (#119)
    * [uss_qualifier] Add rid mock v22a and switch local_test default rid version to v22a
    
    * Switch tracer to v22a and remove v19 rid mocks
    
    * Small improvements
    
    * Update configurations and ci.yaml
    
    * Update collect-logs
    
    * Lint
    
    * Port 8084 in not available on github actions host
    
    * Split atproxy deactivation to another PR
    
    * Address PR comments
    
    * Reorganize test actions
    
    * Remove mock_uss_scdsc2
    
    * Switch default RID to v22a
    
    * reverse mock_uss ports v19-v22a
    
    ---------
    
    Co-authored-by: Mickaël Misbach <mickael@misba.ch>

--- monitoring/mock_uss/run_locally_ridsp_v19.sh
+++ monitoring/mock_uss/run_locally_ridsp_v19.sh
@@ -5,8 +5,7 @@ if [ -z "${DO_NOT_BUILD_MONITORING}" ]; then
   "${SCRIPT_DIR}/../build.sh" || exit 1
 fi
 
-MOCK_CONTAINER_NAME="mock_uss_tracer_v22a" \
-MOCK_USS_RID_VERSION="F3411-22a" \
-MOCK_USS_DSS_URL="http://host.docker.internal:8082" \
-PORT=8088 \
-"${SCRIPT_DIR}/run_locally_tracer.sh" "$@"
+MOCK_CONTAINER_NAME="mock_uss_ridsp_v19" \
+MOCK_USS_RID_VERSION="F3411-19" \
+PORT=8071 \
+"${SCRIPT_DIR}/run_locally_ridsp.sh" "$@"
--- monitoring/mock_uss/start_all_local_mocks.sh
+++ monitoring/mock_uss/start_all_local_mocks.sh
@@ -12,15 +12,21 @@ else
 fi
 cd "${BASEDIR}/../.." || exit 1
 
+RID_VERSION=${RID_VERSION:-"F3411-22a"}
+RID_EXT=""
+if [ "$RID_VERSION" = "F3411-19" ]; then
+	RID_EXT="_v19"
+fi
+
 monitoring/mock_uss/run_locally_scdsc.sh -d
 export DO_NOT_BUILD_MONITORING=true
-monitoring/mock_uss/run_locally_ridsp.sh -d
-monitoring/mock_uss/run_locally_riddp.sh -d
+monitoring/mock_uss/run_locally_ridsp${RID_EXT}.sh -d
+monitoring/mock_uss/run_locally_riddp${RID_EXT}.sh -d
 monitoring/mock_uss/run_locally_geoawareness.sh -d
 monitoring/mock_uss/run_locally_atproxy_client.sh -d
-monitoring/mock_uss/run_locally_tracer.sh -d
+monitoring/mock_uss/run_locally_tracer${RID_EXT}.sh -d
 monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_scdsc
-monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_ridsp
-monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_riddp
+monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_ridsp${RID_EXT}
+monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_riddp${RID_EXT}
 monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_geoawareness
-monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_tracer
+monitoring/mock_uss/wait_for_mock_uss.sh mock_uss_tracer${RID_EXT}
