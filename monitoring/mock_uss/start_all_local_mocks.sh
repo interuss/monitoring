44b8ec7e0fc7f6f54bc9225690d0acb02e19e199
Michael Barroco <michael@orbitalize.com>
2023-07-12 10:59:11 +0200

    Split atproxy deactivation to another PR

--- monitoring/mock_uss/start_all_local_mocks.sh
+++ monitoring/mock_uss/start_all_local_mocks.sh
@@ -20,7 +20,6 @@ fi
 
 monitoring/mock_uss/run_locally_scdsc.sh -d
 export DO_NOT_BUILD_MONITORING=true
-PORT=8094 CONTAINER_NAME=mock_uss_scdsc2 monitoring/mock_uss/run_locally_scdsc.sh -d
 monitoring/mock_uss/run_locally_ridsp${RID_EXT}.sh -d
 monitoring/mock_uss/run_locally_riddp${RID_EXT}.sh -d
 monitoring/mock_uss/run_locally_geoawareness.sh -d
