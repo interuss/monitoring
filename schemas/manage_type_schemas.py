426b92cafa4bfbee3e5cf17eb076757c0ea69b17
Benjamin Pelletier <pelletierb@wing.com>
2023-07-26 22:13:13 +0000

    Add auto-generation of JSON Schema

---
+++ schemas/manage_type_schemas.py
@@ -0,0 +1,195 @@
+import argparse
+import enum
+import inspect
+import json
+import os
+import sys
+from typing import Optional, Set, Dict, Type
+
+from implicitdict import ImplicitDict
+import implicitdict.jsonschema
+from implicitdict.jsonschema import SchemaVars, SchemaVarsResolver
+from loguru import logger
+
+import monitoring
+from monitoring.uss_qualifier import configurations, reports
+from monitoring.monitorlib.inspection import import_submodules
+
+
+class Action(str, enum.Enum):
+    Check = "Check"
+    Generate = "Generate"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Manage auto-generated JSON Schemas")
+
+    parser.add_argument(
+        "--generate",
+        dest="action",
+        action="store_const",
+        const=Action.Generate,
+        help="Create, update, and delete JSON Schema files to match code objects",
+    )
+
+    parser.add_argument(
+        "--check",
+        dest="action",
+        action="store_const",
+        const=Action.Check,
+        help="Just check whether JSON Schema files match code objects",
+    )
+
+    return parser.parse_args()
+
+
+def _find_type_schemas(
+    module,
+    reference_resolver: SchemaVarsResolver,
+    repo: Optional[Dict[str, dict]] = None,
+    already_checked: Optional[Set[str]] = None,
+) -> Dict[str, dict]:
+    if already_checked is None:
+        already_checked = set()
+    already_checked.add(module.__name__)
+
+    if repo is None:
+        repo = {}
+
+    for name, member in inspect.getmembers(module):
+        if (
+            inspect.ismodule(member)
+            and member.__name__ not in already_checked
+            and member.__name__.startswith("monitoring")
+        ):
+            _find_type_schemas(member, reference_resolver, repo, already_checked)
+        elif (
+            inspect.isclass(member)
+            and issubclass(member, ImplicitDict)
+            and member != ImplicitDict
+        ):
+            implicitdict.jsonschema.make_json_schema(member, reference_resolver, repo)
+
+    return repo
+
+
+def main() -> int:
+    args = parse_args()
+
+    if args.action is None:
+        raise ValueError(
+            "Invalid usage; action must be specified with --check or --generate flags"
+        )
+
+    def schema_vars_resolver(schema_type: Type) -> SchemaVars:
+        if schema_type.__module__ in {"builtins", "typing"}:
+            return SchemaVars(name=schema_type.__name__)
+
+        def path_of_py_file(t: Type) -> str:
+            top_module = t.__module__.split(".")[0]
+            module_path = os.path.dirname(sys.modules[top_module].__file__)
+            py_file_path = inspect.getfile(t)
+            return os.path.join(
+                top_module, os.path.relpath(py_file_path, start=module_path)
+            )
+
+        def full_name(t: Type) -> str:
+            return t.__module__ + "." + t.__qualname__
+
+        def path_of_schema_file(t: Type) -> str:
+            return "schemas/" + "/".join(full_name(t).split(".")) + ".json"
+
+        def path_to(t_dest: Type, t_src: Type) -> str:
+            path_to_dest = path_of_schema_file(t_dest)
+            path_to_src = os.path.dirname(path_of_schema_file(t_src))
+            rel_path = os.path.relpath(path_to_dest, start=path_to_src)
+            if rel_path[0] != ".":
+                rel_path = os.path.join("", rel_path)
+            return rel_path
+
+        rel_path = path_of_schema_file(schema_type)
+
+        return SchemaVars(
+            name=rel_path,
+            path_to=path_to,
+            schema_id="https://github.com/interuss/monitoring/blob/main/" + rel_path,
+            description=f"{full_name(schema_type)}, as defined in {path_of_py_file(schema_type)}",
+        )
+
+    for module in (configurations, reports):
+        import_submodules(module)
+    schemas = _find_type_schemas(monitoring, schema_vars_resolver)
+
+    repo_root = os.path.abspath(
+        os.path.join(os.path.dirname(monitoring.__file__), "..")
+    )
+
+    changes = 0
+
+    # Check for non-current schemas that need to be removed
+    for dirpath, _, filenames in os.walk(os.path.join(repo_root, "schemas")):
+        for filename in filenames:
+            rel_filename = os.path.relpath(
+                os.path.join(dirpath, filename), start=repo_root
+            )
+            if not rel_filename.lower().endswith(".json"):
+                continue
+            if rel_filename in schemas:
+                continue
+
+            if args.action == Action.Check:
+                logger.error(
+                    f"{rel_filename} does not correspond with a current code object"
+                )
+                changes += 1
+            elif args.action == Action.Generate:
+                logger.info(f"Removing {rel_filename}")
+                os.remove(os.path.join(dirpath, filename))
+                changes += 1
+            else:
+                raise NotImplementedError(
+                    f"Action {args.action} is not yet implemented"
+                )
+
+    # Check/update every current schema
+    for rel_filename, schema in schemas.items():
+        filename = os.path.abspath(os.path.join(repo_root, rel_filename))
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                old_value = json.load(f)
+            if schema == old_value:
+                continue
+        changes += 1
+
+        if args.action == Action.Check:
+            logger.error(f"{rel_filename} schema does not match current code object")
+        elif args.action == Action.Generate:
+            dir_name = os.path.dirname(
+                os.path.abspath(os.path.join(repo_root, filename))
+            )
+            os.makedirs(dir_name, exist_ok=True)
+            logger.info(f"Updating {rel_filename}")
+            with open(filename, "w") as f:
+                json.dump(schema, f, indent=2)
+        else:
+            raise NotImplementedError(f"Action {args.action} is not yet implemented")
+
+    # Terminate appropriately depending on action and outcome
+    if changes > 0:
+        if args.action == Action.Check:
+            logger.error(
+                f"JSON Schema updates are necessary for {changes} files; run `make format` to update them."
+            )
+            return -1
+        elif args.action == Action.Generate:
+            logger.info(f"JSON Schema updated successfully for {changes} files.")
+        else:
+            raise NotImplementedError(f"Action {args.action} is not yet implemented")
+    else:
+        logger.info("No JSON Schema updates are necessary.")
+
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ schemas/manage_type_schemas.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+set -o xtrace
+
+# Find and change to repo root directory
+OS=$(uname)
+if [[ "$OS" == "Darwin" ]]; then
+	# OSX uses BSD readlink
+	BASEDIR="$(dirname "$0")"
+else
+	BASEDIR=$(readlink -e "$(dirname "$0")")
+fi
+cd "${BASEDIR}/.." || exit 1
+
+monitoring/build.sh || exit 1
+
+action=${1:?The action must be specified as --check or --generate}
+
+# shellcheck disable=SC2086
+docker run --name type_schema_manager \
+  --rm \
+  -v "$(pwd):/app" \
+  interuss/monitoring \
+  python /app/schemas/manage_type_schemas.py "${action}"
