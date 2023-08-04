4a50c425323a2119b71f040f0f17b693907694a0
Julien Perrochet <julien.perro@gmail.com>
2023-08-04 16:52:36 +0200

    [rid] Type hints improvement and proper participants forwarding (#148)
    Improve on some type hints, rename accordingly and correctly pass participants to recorded failure in evaluate_uas_id

--- monitoring/uss_qualifier/scenarios/astm/netrid/display_data_evaluator.py
+++ monitoring/uss_qualifier/scenarios/astm/netrid/display_data_evaluator.py
@@ -797,7 +797,7 @@ class RIDObservationEvaluator(object):
                         ),
                         query_timestamps=[flights_query.query.request.timestamp],
                     )
-            self._common_dictionary_evaluator.evaluate_sp_flights_response(
+            self._common_dictionary_evaluator.evaluate_sp_flights(
                 sp_observation,
                 participants=[mapping.injected_flight.uss_participant_id],
             )
@@ -869,8 +869,8 @@ class RIDObservationEvaluator(object):
                         query_timestamps=[details_query.query.request.timestamp],
                     )
 
-            self._common_dictionary_evaluator.evaluate_sp_details_response(
-                details_query, [mapping.injected_flight.uss_participant_id]
+            self._common_dictionary_evaluator.evaluate_sp_details(
+                details_query.details, [mapping.injected_flight.uss_participant_id]
             )
 
     def _evaluate_area_too_large_sp_observation(
