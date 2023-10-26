cb5c1d470d188c0b968c6ea83b0ffa0502eb67f3
Julien Perrochet <julien.perrochet@orbitalize.com>
2023-10-26 17:16:07 +0200

    [uss_qualifier] DSS0030 migrate isa expiry check from prober (#288)

--- monitoring/uss_qualifier/scenarios/astm/netrid/v22a/dss/__init__.py
+++ monitoring/uss_qualifier/scenarios/astm/netrid/v22a/dss/__init__.py
@@ -1,4 +1,5 @@
 from .isa_simple import ISASimple
 from .isa_validation import ISAValidation
+from .isa_expiry import ISAExpiry
 from .subscription_validation import SubscriptionValidation
 from .crdb_access import CRDBAccess
