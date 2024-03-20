1ca6a5b925db73a639381b03a30bf2d89b70e4d1
Julien Perrochet <julien.perrochet@orbitalize.com>
2024-03-20 14:54:11 +0100

    [uss_qualifier] move SCD dss-specific scenarios to DSS subdir (#559)
    [uss_qualidier] move dss-specific test scenarios to the dss subdirectory

--- monitoring/uss_qualifier/scenarios/astm/utm/__init__.py
+++ monitoring/uss_qualifier/scenarios/astm/utm/__init__.py
@@ -5,11 +5,9 @@ from .nominal_planning.conflict_higher_priority.conflict_higher_priority import 
 from .nominal_planning.conflict_equal_priority_not_permitted.conflict_equal_priority_not_permitted import (
     ConflictEqualPriorityNotPermitted,
 )
-from .dss_interoperability import DSSInteroperability
 from .aggregate_checks import AggregateChecks
 from .prep_planners import PrepareFlightPlanners
 from .off_nominal_planning.down_uss import DownUSS
 from .off_nominal_planning.down_uss_equal_priority_not_permitted import (
     DownUSSEqualPriorityNotPermitted,
 )
-from .op_intent_ref_access_control import OpIntentReferenceAccessControl
--- monitoring/uss_qualifier/scenarios/astm/utm/dss/__init__.py
+++ monitoring/uss_qualifier/scenarios/astm/utm/dss/__init__.py
@@ -1,3 +1,5 @@
 from .subscription_validation import SubscriptionValidation
 from .subscription_simple import SubscriptionSimple
 from .crdb_access import CRDBAccess
+from .op_intent_ref_access_control import OpIntentReferenceAccessControl
+from .dss_interoperability import DSSInteroperability
