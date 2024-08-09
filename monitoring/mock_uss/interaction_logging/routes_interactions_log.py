c53fe62cc495b899617547e51d3db74609405400
Julien Perrochet <julien.perrochet@orbitalize.com>
2024-08-09 14:50:09 +0200

    [mock_uss] avoid full file scan when querying interaction log

--- monitoring/mock_uss/interaction_logging/routes_interactions_log.py
+++ monitoring/mock_uss/interaction_logging/routes_interactions_log.py
@@ -1,30 +1,33 @@
-from typing import Tuple, List
+import datetime
 import json
 import os
+from typing import Tuple, List
+
+from flask import request, jsonify, Response
 from implicitdict import ImplicitDict, StringBasedDateTime
-from flask import request, jsonify
 from loguru import logger
 
 from monitoring.mock_uss import webapp
 from monitoring.mock_uss.auth import requires_scope
-from monitoring.monitorlib.scd_automated_testing.scd_injection_api import (
-    SCOPE_SCD_QUALIFIER_INJECT,
-)
+from monitoring.mock_uss.interaction_logging.config import KEY_INTERACTIONS_LOG_DIR
 from monitoring.monitorlib.clients.mock_uss.interactions import (
     Interaction,
     ListLogsResponse,
 )
+from monitoring.monitorlib.scd_automated_testing.scd_injection_api import (
+    SCOPE_SCD_QUALIFIER_INJECT,
+)
 
-from monitoring.mock_uss.interaction_logging.config import KEY_INTERACTIONS_LOG_DIR
+CLIENT_CLOCK_SKEW_BUFFER = datetime.timedelta(seconds=5)
 
 
 @webapp.route("/mock_uss/interuss_logging/logs", methods=["GET"])
 @requires_scope(SCOPE_SCD_QUALIFIER_INJECT)
-def interaction_logs() -> Tuple[str, int]:
+def interaction_logs() -> Tuple[Response, int]:
     """
     Returns all the interaction logs with requests that were
     received or initiated between 'from_time' and now
-    Eg - http:/.../mock_uss/interuss/log?from_time=2023-08-30T20:48:21.900000Z
+    Eg - http:/.../mock_uss/interuss_logging/logs?from_time=2023-08-30T20:48:21.900000Z
     """
     from_time_param = request.args.get("from_time", "1900-01-01T00:00:00Z")
     from_time = StringBasedDateTime(from_time_param)
@@ -33,8 +36,25 @@ def interaction_logs() -> Tuple[str, int]:
     if not os.path.exists(log_path):
         raise ValueError(f"Configured log path {log_path} does not exist")
 
+    # individual interactions are logged to a file of the form <index>_<direction>_<method>_<timestamp>.json, eg
+    # 000001_Incoming_GET_2023-08-30T20:48:21.900000Z.json
     interactions: List[Interaction] = []
     for fname in os.listdir(log_path):
+        # Parse the interaction time from the file name:
+        fname_parts = fname.split("_")
+        if len(fname_parts) != 4:
+            # We may just want to fail hard here to let users know something is likely very wrong
+            logger.warning(
+                f"Skipping file {fname} as it does not match the expected format"
+            )
+            continue
+        interaction_time_str = fname_parts[3].split(".")[0]
+        interaction_time = StringBasedDateTime(interaction_time_str)
+        # Add a buffer to look slightly before the from_time to account for possible clock skew and the fact
+        # that the timestamp used in the filename is based on the mock_uss clock, not the event time
+        # from the perspective of the client.
+        if interaction_time.datetime < (from_time.datetime - CLIENT_CLOCK_SKEW_BUFFER):
+            continue
         with open(os.path.join(log_path, fname), "r") as f:
             try:
                 obj = json.load(f)
