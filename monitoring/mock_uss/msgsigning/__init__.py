499f56d6261fdee41152cec09af344403d4819bf
Benjamin Pelletier <pelletierb@wing.com>
2025-10-15 15:25:58 +0000

    Re-remove broken message signing content

--- monitoring/mock_uss/msgsigning/__init__.py
+++
@@ -1,6 +0,0 @@
-from monitoring.mock_uss import config
-
-if not config.Config.CERT_BASE_PATH:
-    raise ValueError(
-        f"Environment variable {config.ENV_KEY_CERT_BASE_PATH} may not be blank for the message signing functionality"
-    )
