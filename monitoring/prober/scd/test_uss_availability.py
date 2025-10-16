866643e4613ed3fa5cb92544246ea004d3f0d327
Michael Barroco <michael@orbitalize.com>
2025-10-16 21:51:03 +0200

    [prober] uss availability - support multiple run

--- monitoring/prober/scd/test_uss_availability.py
+++ monitoring/prober/scd/test_uss_availability.py
@@ -3,12 +3,26 @@ from monitoring.monitorlib.scd import SCOPE_AA
 from monitoring.prober.infrastructure import depends_on
 
 
+def _get_uss_availability(uss_id, scd_session2):
+    resp = scd_session2.get(f"/uss_availability/{uss_id}", scope=SCOPE_AA)
+    assert resp.status_code == 200, resp.content
+    data = resp.json()
+    return data["status"]["uss"], data["status"]["availability"], data["version"]
+
+
 @default_scope(SCOPE_AA)
 def test_set_uss_availability(ids, scd_session2):
     resp = scd_session2.put(
         "/uss_availability/uss1", scope=SCOPE_AA, json={"availability": "normal"}
     )
-    assert resp.status_code == 200, resp.content
+
+    if resp.status_code == 409:
+      # record already exists, retrieving version to mutate
+      _, _, version = _get_uss_availability("uss1", scd_session2)
+      resp = scd_session2.put(
+        "/uss_availability/uss1", scope=SCOPE_AA, json={"availability": "normal", "old_version": version}
+      )
+
     data = resp.json()
     assert data["status"]["uss"] == "uss1"
     assert data["status"]["availability"] == "Normal"
@@ -23,15 +37,12 @@ def test_set_uss_availability(ids, scd_session2):
 @default_scope(SCOPE_AA)
 @depends_on(test_set_uss_availability)
 def test_get_uss_availability(ids, scd_session2):
-    resp = scd_session2.get("/uss_availability/unknown_uss2", scope=SCOPE_AA)
-    assert resp.status_code == 200, resp.content
-    data = resp.json()
-    assert data["status"]["availability"] == "Unknown"
-    assert data["version"] == ""
+    uss, availability, version = _get_uss_availability("unknown_uss2", scd_session2)
+    assert uss == "unknown_uss2"
+    assert availability == "Unknown"
+    assert version == ""
 
-    resp = scd_session2.get("/uss_availability/uss1", scope=SCOPE_AA)
-    assert resp.status_code == 200, resp.content
-    data = resp.json()
-    assert data["status"]["uss"] == "uss1"
-    assert data["status"]["availability"] == "Normal"
-    assert data["version"]
+    uss, availability, version = _get_uss_availability("uss1", scd_session2)
+    assert uss == "uss1"
+    assert availability == "Normal"
+    assert version
