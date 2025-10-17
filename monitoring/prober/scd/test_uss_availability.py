e72c05528c3c7316360fb14f900addb91964e5d8
Michael Barroco <michael@orbitalize.com>
2025-10-17 09:13:43 +0200

    Refactor

--- monitoring/prober/scd/test_uss_availability.py
+++ monitoring/prober/scd/test_uss_availability.py
@@ -12,39 +12,69 @@ def _get_uss_availability(uss_id, scd_session2):
 
 @default_scope(SCOPE_AA)
 def test_set_uss_availability(ids, scd_session2):
-    resp = scd_session2.put(
-        "/uss_availability/uss1", scope=SCOPE_AA, json={"availability": "normal"}
-    )
+    # There is no interface to clear a uss availability status.
+    # Therefore we always need to check if a status already exists.
+    _, _, version = _get_uss_availability("uss1", scd_session2)
 
-    if resp.status_code == 409:
-        # record already exists, retrieving version to mutate
-        _, _, version = _get_uss_availability("uss1", scd_session2)
-        resp = scd_session2.put(
-            "/uss_availability/uss1",
-            scope=SCOPE_AA,
-            json={"availability": "normal", "old_version": version},
-        )
+    payload = {"availability": "normal"}
+    if version:
+        payload["old_version"] = version
 
+    resp = scd_session2.put("/uss_availability/uss1", scope=SCOPE_AA, json=payload)
+
+    assert resp.status_code == 200, resp.content
     data = resp.json()
     assert data["status"]["uss"] == "uss1"
     assert data["status"]["availability"] == "Normal"
     assert data["version"]
 
+    uss, availability, version = _get_uss_availability("uss1", scd_session2)
+    assert uss == "uss1"
+    assert availability == "Normal"
+    assert version == data["version"]
+
+
+@default_scope(SCOPE_AA)
+@depends_on(test_set_uss_availability)
+def test_mutate_uss_availability(ids, scd_session2):
+    _, _, version = _get_uss_availability("uss1", scd_session2)
+
+    resp = scd_session2.put(
+        "/uss_availability/uss1",
+        scope=SCOPE_AA,
+        json={"availability": "down", "old_version": version},
+    )
+
+    assert resp.status_code == 200, resp.content
+    data = resp.json()
+    assert data["status"]["uss"] == "uss1"
+    assert data["status"]["availability"] == "Down"
+    assert data["version"]
+
+    uss, availability, version = _get_uss_availability("uss1", scd_session2)
+    assert uss == "uss1"
+    assert availability == "Down"
+    assert version == data["version"]
+
+
+@default_scope(SCOPE_AA)
+def test_set_invalid_uss_availability(ids, scd_session2):
     resp = scd_session2.put(
         "/uss_availability/uss1", scope=SCOPE_AA, json={"availability": "pUrPlE"}
     )
     assert resp.status_code == 400, resp.content
 
+    resp = scd_session2.put(
+        "/uss_availability/uss1",
+        scope=SCOPE_AA,
+        json={"availability": "normal", "old_version": "invalid"},
+    )
+    assert resp.status_code == 409, resp.content
+
 
 @default_scope(SCOPE_AA)
-@depends_on(test_set_uss_availability)
-def test_get_uss_availability(ids, scd_session2):
+def test_get_unknown_uss_availability(ids, scd_session2):
     uss, availability, version = _get_uss_availability("unknown_uss2", scd_session2)
     assert uss == "unknown_uss2"
     assert availability == "Unknown"
     assert version == ""
-
-    uss, availability, version = _get_uss_availability("uss1", scd_session2)
-    assert uss == "uss1"
-    assert availability == "Normal"
-    assert version
