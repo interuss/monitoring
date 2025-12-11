4e923dd91bb4398f8fec068cd9121082bc29bfcc
Mickaël Misbach <mickael@misba.ch>
2025-12-11 11:59:17 +0100

    [datastore_access] split crdb and ybdb logic; correct ybdb implementation

--- monitoring/uss_qualifier/resources/interuss/datastore/datastore_test.py
+++ monitoring/uss_qualifier/resources/interuss/datastore/datastore_test.py
@@ -2,50 +2,65 @@ import pytest
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.wait_strategies import LogMessageWaitStrategy
 
-from . import DatastoreDBNode
+from . import CockroachDBNode, YugabyteDBNode
+
+COCKROACHDB_IMAGE = "cockroachdb/cockroach:v24.1.3"
+INSECURE_COCKROACHDB_IMAGE = "interuss/insecurecockroach:latest"
+YUGABYTE_IMAGE = "interuss/yugabyte:2025.1.2.1-interuss"
+INSECURE_YUGABYTE_IMAGE = "yugabytedb/yugabyte:2.25.2.0-b359"
 
 
 @pytest.fixture(scope="module")
 def good_cockroach(request):
     server = DockerContainer(
-        image="cockroachdb/cockroach:v24.1.3",
+        image=COCKROACHDB_IMAGE,
         ports=[26257],
         command="start-single-node",
     )
     server.waiting_for(LogMessageWaitStrategy("start_node_query"))
     server.start()
 
-    return DatastoreDBNode(
+    return CockroachDBNode(
         "test", server.get_container_host_ip(), server.get_exposed_port(26257)
     )
 
 
 @pytest.fixture(scope="module")
-def no_ssl_cockroach(request):
+def not_running_cockroach(request):
+    return CockroachDBNode("test", "127.0.0.1", 1)
+
+
+@pytest.fixture(scope="module")
+def not_running_yugabyte(request):
+    return YugabyteDBNode("test", "127.0.0.1", 1)
+
+
+@pytest.fixture(scope="module")
+def no_tls_cockroach(request):
     server = DockerContainer(
-        image="cockroachdb/cockroach:v24.1.3",
+        image=COCKROACHDB_IMAGE,
         ports=[26257],
         command="start-single-node --insecure",
     )
     server.waiting_for(LogMessageWaitStrategy("start_node_query"))
     server.start()
 
-    return DatastoreDBNode(
+    return CockroachDBNode(
         "test", server.get_container_host_ip(), server.get_exposed_port(26257)
     )
 
 
 @pytest.fixture(scope="module")
-def old_ssl_cockroach(request):
+def old_tls_cockroach(request):
     server = DockerContainer(
-        image="interuss/insecurecockroach:latest",
+        image=INSECURE_COCKROACHDB_IMAGE,
         ports=[26257],
         command="start-single-node",
     )
     server.waiting_for(LogMessageWaitStrategy("start_node_query"))
     server.start()
 
-    return DatastoreDBNode(
+    return CockroachDBNode(
         "test", server.get_container_host_ip(), server.get_exposed_port(26257)
     )
 
@@ -53,25 +68,42 @@ def old_ssl_cockroach(request):
 @pytest.fixture(scope="module")
 def good_yugabyte(request):
     server = DockerContainer(
-        image="yugabytedb/yugabyte:2.25.2.0-b359",
-        ports=[5433],
-        command='bash -c "bin/yugabyted cert generate_server_certs --base_dir /yugabyte/certs --hostnames `hostname`  && bin/yugabyted start --secure --certs_dir=/yugabyte/certs/generated_certs/`hostname` --advertise_address=`hostname` --background=false"',
+        image=YUGABYTE_IMAGE,
+        ports=[7100],
+        command='bash -c "bin/yugabyted cert generate_server_certs --base_dir /yugabyte/certs --hostnames `hostname` && bin/yugabyted start --secure --certs_dir=/yugabyte/certs/generated_certs/`hostname` --advertise_address=`hostname` --background=false --tserver_flags=node_to_node_encryption_use_client_certificates=true --master_flags=node_to_node_encryption_use_client_certificates=true,use_node_to_node_encryption=true"',
     )
     server.waiting_for(
         LogMessageWaitStrategy("Data placement constraint successfully verified")
     )
     server.start()
 
-    return DatastoreDBNode(
-        "test", server.get_container_host_ip(), server.get_exposed_port(5433)
+    return YugabyteDBNode(
+        "test", server.get_container_host_ip(), server.get_exposed_port(7100)
     )
 
 
 @pytest.fixture(scope="module")
-def no_ssl_yugabyte(request):
+def yugabyte_without_client_auth(request):
     server = DockerContainer(
-        image="yugabytedb/yugabyte:2.25.2.0-b359",
-        ports=[5433],
+        image=INSECURE_YUGABYTE_IMAGE,
+        ports=[7100],
+        command='bash -c "bin/yugabyted cert generate_server_certs --base_dir /yugabyte/certs --hostnames `hostname` && bin/yugabyted start --secure --certs_dir=/yugabyte/certs/generated_certs/`hostname` --advertise_address=`hostname` --background=false"',
+    )
+    server.waiting_for(
+        LogMessageWaitStrategy("Data placement constraint successfully verified")
+    )
+    server.start()
+
+    return YugabyteDBNode(
+        "test", server.get_container_host_ip(), server.get_exposed_port(7100)
+    )
+
+
+@pytest.fixture(scope="module")
+def no_tls_yugabyte(request):
+    server = DockerContainer(
+        image=INSECURE_YUGABYTE_IMAGE,
+        ports=[7100],
         command="bin/yugabyted start --background=false",
     )
     server.waiting_for(
@@ -79,108 +111,163 @@ def no_ssl_yugabyte(request):
     )
     server.start()
 
-    return DatastoreDBNode(
-        "test", server.get_container_host_ip(), server.get_exposed_port(5433)
-    )
-
-
-@pytest.fixture(scope="module")
-def old_ssl_yugabyte(request):
-    import base64
-
-    config = '{"tserver_flags": "ysql_pg_conf_csv=\\"ssl_min_protocol_version=\'TLSv1.1\'\\""}'
-    config = base64.b64encode(config.encode("utf-8"))  # Avoid escaping hell
-
-    server = DockerContainer(
-        image="yugabytedb/yugabyte:2.25.2.0-b359",
-        ports=[5433],
-        command=f'bash -c "echo {config.decode("utf-8")} | base64 -d > /conf.conf && cat /conf.conf && bin/yugabyted cert generate_server_certs --base_dir /yugabyte/certs --hostnames `hostname` && bin/yugabyted start --secure --certs_dir=/yugabyte/certs/generated_certs/`hostname` --advertise_address=`hostname` --background=false --conf /conf.conf"',
-    )
-    server.waiting_for(
-        LogMessageWaitStrategy("Data placement constraint successfully verified")
-    )
-    server.start()
-
-    return DatastoreDBNode(
-        "test", server.get_container_host_ip(), server.get_exposed_port(5433)
+    return YugabyteDBNode(
+        "test", server.get_container_host_ip(), server.get_exposed_port(7100)
     )
 
 
 def test_datastoredbmode_connect_good_cockroach(good_cockroach):
     is_reachable, _ = good_cockroach.is_reachable()
-    assert is_reachable
+    assert is_reachable, "Running CockroachDB server shall be reachable"
 
 
 def test_datastoredbmode_connect_good_yugabyte(good_yugabyte):
     is_reachable, _ = good_yugabyte.is_reachable()
-    assert is_reachable
+    assert is_reachable, "Running Yugabyte server shall be reachable"
 
 
-def test_datastoredbmode_connect_no_ssl_cockroach(no_ssl_cockroach):
-    is_reachable, _ = no_ssl_cockroach.is_reachable()
-    assert is_reachable
+def test_datastoredbmode_connect_yugabyte_without_client_auth(
+    yugabyte_without_client_auth,
+):
+    is_reachable, _ = yugabyte_without_client_auth.is_reachable()
+    assert is_reachable, "Running Yugabyte server shall be reachable"
 
 
-def test_datastoredbmode_connect_no_ssl_yugabyte(no_ssl_yugabyte):
-    is_reachable, _ = no_ssl_yugabyte.is_reachable()
-    assert is_reachable
+def test_datastoredbmode_connect_no_tls_cockroach(no_tls_cockroach):
+    is_reachable, _ = no_tls_cockroach.is_reachable()
+    assert is_reachable, "Running CockroachDB server shall be reachable"
 
 
-def test_datastoredbmode_connect_old_ssl_cockroach(old_ssl_cockroach):
-    is_reachable, _ = old_ssl_cockroach.is_reachable()
-    assert is_reachable
+def test_datastoredbmode_connect_no_tls_yugabyte(no_tls_yugabyte):
+    is_reachable, _ = no_tls_yugabyte.is_reachable()
+    assert is_reachable, "Running Yugabyte server shall be reachable"
 
 
-def test_datastoredbmode_connect_old_ssl_yugabyte(old_ssl_yugabyte):
-    is_reachable, _ = old_ssl_yugabyte.is_reachable()
-    assert is_reachable
+def test_datastoredbmode_connect_not_running_cockroach(not_running_cockroach):
+    is_reachable, _ = not_running_cockroach.is_reachable()
+    assert not is_reachable, "Non-running CockroachDB server shall not be reachable"
 
 
-def test_datastoredbmode_secure_mode_good_cockroach(good_cockroach):
-    is_secure, _ = good_cockroach.runs_in_secure_mode()
-    assert is_secure
+def test_datastoredbmode_connect_not_running_yugabyte(not_running_yugabyte):
+    is_reachable, _ = not_running_yugabyte.is_reachable()
+    assert not is_reachable, "Non-running Yugabyte server shall not be reachable"
 
 
-def test_datastoredbmode_secure_mode_good_yugabyte(good_yugabyte):
-    is_secure, _ = good_yugabyte.runs_in_secure_mode()
-    assert is_secure
+def test_datastoredbmode_no_tls_rejected_good_cockroach(good_cockroach):
+    no_tls_rejected, _ = good_cockroach.no_tls_rejected()
+    assert no_tls_rejected, (
+        "Nominal CockroachDB server shall reject connections without TLS"
+    )
 
 
-def test_datastoredbmode_secure_mode_no_ssl_cockroach(no_ssl_cockroach):
-    is_secure, _ = no_ssl_cockroach.runs_in_secure_mode()
-    assert not is_secure
+def test_datastoredbmode_no_tls_rejected_good_yugabyte(good_yugabyte):
+    no_tls_rejected, _ = good_yugabyte.no_tls_rejected()
+    assert no_tls_rejected, (
+        "Nominal Yugabyte server shall reject connections without TLS"
+    )
 
 
-def test_datastoredbmode_secure_mode_no_ssl_yugabyte(no_ssl_yugabyte):
-    is_secure, _ = no_ssl_yugabyte.runs_in_secure_mode()
-    assert not is_secure
+def test_datastoredbmode_no_tls_rejected_yugabyte_without_client_auth(
+    yugabyte_without_client_auth,
+):
+    no_tls_rejected, _ = yugabyte_without_client_auth.no_tls_rejected()
+    assert no_tls_rejected, (
+        "Yugabyte server without mTLS shall still reject connections without TLS"
+    )
 
 
-def test_datastoredbmode_secure_mode_old_ssl_cockroach(old_ssl_cockroach):
-    is_secure, _ = old_ssl_cockroach.runs_in_secure_mode()
-    assert is_secure
+def test_datastoredbmode_no_tls_rejected_no_tls_cockroach(no_tls_cockroach):
+    no_tls_rejected, _ = no_tls_cockroach.no_tls_rejected()
+    assert not no_tls_rejected, (
+        "CockroachDB server, with TLS disabled, shall not reject connections without TLS"
+    )
 
 
-def test_datastoredbmode_secure_mode_old_ssl_yugabyte(old_ssl_yugabyte):
-    is_secure, _ = old_ssl_yugabyte.runs_in_secure_mode()
-    assert is_secure
+def test_datastoredbmode_no_tls_rejected_no_tls_yugabyte(no_tls_yugabyte):
+    no_tls_rejected, _ = no_tls_yugabyte.no_tls_rejected()
+    assert not no_tls_rejected, (
+        "Yugabyte server, with TLS disabled, shall not reject connections without TLS"
+    )
+
+
+def test_datastoredbmode_no_tls_rejected_old_tls_cockroach(old_tls_cockroach):
+    no_tls_rejected, _ = old_tls_cockroach.no_tls_rejected()
+    assert no_tls_rejected, (
+        "CockroachDB server, with old TLS version enabled, shall not reject connections without TLS"
+    )
+
+
+def test_datastoredbmode_unauthenticated_rejected_good_cockroach(good_cockroach):
+    unauthenticated_rejected, _ = good_cockroach.unauthenticated_rejected()
+    assert unauthenticated_rejected, (
+        "Nominal CockroachDB server shall reject unauthenticated connections"
+    )
+
+
+def test_datastoredbmode_unauthenticated_rejected_good_yugabyte(good_yugabyte):
+    unauthenticated_rejected, _ = good_yugabyte.unauthenticated_rejected()
+    assert unauthenticated_rejected, (
+        "Nominal Yugabyte server shall reject unauthenticated connections"
+    )
+
+
+def test_datastoredbmode_unauthenticated_rejected_yugabyte_without_client_auth(
+    yugabyte_without_client_auth,
+):
+    unauthenticated_rejected, _ = (
+        yugabyte_without_client_auth.unauthenticated_rejected()
+    )
+    assert not unauthenticated_rejected, (
+        "Yugabyte without mTLS shall accept unauthenticated requests (since mTLS is the authentication method)"
+    )
+
+
+def test_datastoredbmode_unauthenticated_rejected_no_tls_cockroach(no_tls_cockroach):
+    unauthenticated_rejected, _ = no_tls_cockroach.unauthenticated_rejected()
+    assert unauthenticated_rejected, (
+        "CockroachDB server, with TLS disabled, shall reject authenticated requests"
+    )
+
+
+def test_datastoredbmode_unauthenticated_rejected_no_tls_yugabyte(no_tls_yugabyte):
+    unauthenticated_rejected, _ = no_tls_yugabyte.unauthenticated_rejected()
+    assert not unauthenticated_rejected, (
+        "Yugabyte without TLS shall accept unauthenticated requests (since mTLS is the authentication method)"
+    )
+
+
+def test_datastoredbmode_unauthenticated_rejected_old_tls_cockroach(old_tls_cockroach):
+    unauthenticated_rejected, _ = old_tls_cockroach.unauthenticated_rejected()
+    assert unauthenticated_rejected, (
+        "CockroachDB server, with old TLS version enabled, shall reject authenticated requests"
+    )
 
 
 def test_datastoredbmode_reject_legacy_good_cockroach(good_cockroach):
-    legacy_rejected, _ = good_cockroach.legacy_ssl_version_rejected()
-    assert legacy_rejected
+    legacy_rejected, _ = good_cockroach.legacy_tls_version_rejected()
+    assert legacy_rejected, (
+        "Nominal CockroachDB server shall reject connections wtih legacy TLS version"
+    )
 
 
 def test_datastoredbmode_reject_legacy_good_yugabyte(good_yugabyte):
-    legacy_rejected, _ = good_yugabyte.legacy_ssl_version_rejected()
-    assert legacy_rejected
+    legacy_rejected, _ = good_yugabyte.legacy_tls_version_rejected()
+    assert legacy_rejected, (
+        "Nominal Yugabyte server shall reject connections wtih legacy TLS version"
+    )
 
 
-def test_datastoredbmode_reject_legacy_old_ssl_cockroach(old_ssl_cockroach):
-    legacy_rejected, _ = old_ssl_cockroach.legacy_ssl_version_rejected()
-    assert not legacy_rejected
+def test_datastoredbmode_reject_legacy_yugabyte_without_client_auth(
+    yugabyte_without_client_auth,
+):
+    legacy_rejected, _ = yugabyte_without_client_auth.legacy_tls_version_rejected()
+    assert legacy_rejected, (
+        "Yugabyte without mTLS shall reject connections wtih legacy TLS version"
+    )
 
 
-def test_datastoredbmode_reject_legacy_old_ssl_yugabyte(old_ssl_yugabyte):
-    legacy_rejected, _ = old_ssl_yugabyte.legacy_ssl_version_rejected()
-    assert not legacy_rejected
+def test_datastoredbmode_reject_legacy_old_tls_cockroach(old_tls_cockroach):
+    legacy_rejected, _ = old_tls_cockroach.legacy_tls_version_rejected()
+    assert not legacy_rejected, (
+        "CockroachDB server, with old TLS version enabled, shall not reject connections wtih legacy TLS versions"
+    )
--- monitoring/uss_qualifier/scenarios/astm/utm/dss/datastore_access.md
+++ monitoring/uss_qualifier/scenarios/astm/utm/dss/datastore_access.md
@@ -3,7 +3,9 @@
 ## Overview
 
 Attempt to directly access the datastore (CockroachDB or Yugabyte) nodes intercommunicating to form the DSS Airspace Representation for the DSS instances under test, for the purpose of determining compliance to certain DSS interoperability requirements.
-The psycopg library is used to attempt standard PostgreSQL connections to the nodes as it is the most straightforward way of connecting directly to the datastore nodes while controlling the connection parameters (such as for encryption).
+
+For CockroachDB, the psycopg library is used to attempt standard PostgreSQL connections to the nodes as it is the most straightforward way of connecting directly to the datastore nodes while controlling the connection parameters (such as for encryption).
+For YugabyteDB, RPC messages are sent to the nodes directly.
 
 This scenario aims at validating the following requirements:
 - **[astm.f3548.v21.DSS0200](../../../../requirements/astm/f3548/v21.md)**
@@ -15,12 +17,10 @@ DatastoreDBClusterResource that provides access to a set of CockroachDB or Yugab
 
 ## Setup test case
 ### Validate nodes are reachable test step
-Attempt connection with nodes of the cluster to validate that they are reachable.
-To do so, this step attempts a connection to each node without strictly requiring an encrypted connection and forcing the use of a password authentication with a dummy password.
-As such we expect the node to respond with a failed password authentication.
+Attempt connection with nodes of the cluster to validate that they are reachable, by opening a TCP connection to the node.
 
 #### 🛑 Node is reachable check
-This check succeeds if the node can be reached and that the password authentication attempted by the USS qualifier is rejected.
+This check succeeds if the node is reachable.
 It fails in all other cases.
 
 ## Verify security interoperability test case
@@ -28,12 +28,13 @@ It fails in all other cases.
 Attempt connection with nodes of the cluster in insecure mode.
 It is expected that the connection attempts are rejected due to the fact that all nodes are running in secure mode.
 
-#### ⚠️ Node runs in secure mode check
+#### ⚠️ Node enforces encryption of its communications check
 This check succeeds if the node rejects the insecure connection attempt by the USS qualifier because it is in secure mode.
-It fails in all other cases.
-
-If it is in insecure mode, it means that the node does not authenticate incoming connection attempt as it should per **[astm.f3548.v21.DSS0200](../../../../requirements/astm/f3548/v21.md)**.
 If it is in insecure mode, it means that the node does not encrypt its communications as it should per **[astm.f3548.v21.DSS0205](../../../../requirements/astm/f3548/v21.md)**.
+
+#### ⚠️ Node enforces authentication of its communications check
+This check succeeds if the node rejects the insecure connection attempt by the USS qualifier because it is in secure mode.
+If it is in insecure mode, it means that the node does not authenticate incoming connection attempt as it should per **[astm.f3548.v21.DSS0200](../../../../requirements/astm/f3548/v21.md)**.
 
 ### Attempt to connect with legacy encryption protocol test step
 Attempt connection with nodes of the cluster forcing the use of legacy encryption protocols, namely between TLSv1 and TLSv1.1.
