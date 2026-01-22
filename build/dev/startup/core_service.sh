62e770ace99f52090f96fa282976f9e107e37889
Maximilien Cuony <maximilien.cuony@orbitalize.com>
2026-01-22 10:35:25 +0100

    [tooling] Add support for yugabyte in local docker-compose

--- build/dev/startup/core_service.sh
+++ build/dev/startup/core_service.sh
@@ -7,11 +7,22 @@ set -e
 
 DEBUG_ON=${1:-0}
 
+# POSIX compliant test to check if with-yugabyte profile is enabled.
+if [ "${COMPOSE_PROFILES#*"with-yugabyte"}" != "${COMPOSE_PROFILES}" ]; then
+  echo "Using Yugabyte"
+  DATASTORE_CONNECTION="-datastore_host ybdb -datastore_user yugabyte --datastore_port 5433"
+else
+  echo "Using CockroachDB"
+  DATASTORE_CONNECTION="-datastore_host crdb.uss1.localutm"
+fi
+
 if [ "$DEBUG_ON" = "1" ]; then
   echo "Debug Mode: on"
 
+  # Linter is disabled to properly unwrap $DATASTORE_CONNECTION.
+  # shellcheck disable=SC2086
   dlv --headless --listen=:4000 --api-version=2 --accept-multiclient exec --continue /usr/bin/core-service -- \
-  -cockroach_host crdb.uss1.localutm \
+  ${DATASTORE_CONNECTION} \
   -public_key_files /var/test-certs/auth2.pem \
   -log_format console \
   -dump_requests \
@@ -24,8 +35,10 @@ if [ "$DEBUG_ON" = "1" ]; then
 else
   echo "Debug Mode: off"
 
+  # Linter is disabled to properly unwrap $DATASTORE_CONNECTION.
+  # shellcheck disable=SC2086
   /usr/bin/core-service \
-  -cockroach_host crdb.uss1.localutm \
+  ${DATASTORE_CONNECTION} \
   -public_key_files /var/test-certs/auth2.pem \
   -log_format console \
   -dump_requests \
