5afe6f418faa737a6303722c93545ddbdf1f7058
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2026-06-13 02:13:29 +0000

    [uss_qualifier] Add first part of pool_info test scenario implementation (#1487)

--- monitoring/uss_qualifier/scenarios/astm/utm/dss/pool_info.py
+++ monitoring/uss_qualifier/scenarios/astm/utm/dss/pool_info.py
@@ -1,13 +1,13 @@
-from monitoring.uss_qualifier.resources.astm.f3548.v21 import DSSInstancesResource
-from monitoring.uss_qualifier.scenarios.astm.dss.pool_info import (
+from monitoring.uss_qualifier.resources.astm.f3548.v21.dss import DSSInstancesResource
+from monitoring.uss_qualifier.scenarios.interuss.dss.pool_info import (
     PoolInfo as CommonPoolInfo,
 )
 from monitoring.uss_qualifier.scenarios.scenario import TestScenario
 
 
-class PoolInfo(TestScenario, CommonPoolInfo):
+class PoolInfo(CommonPoolInfo, TestScenario):
     def __init__(
         self,
         dss_instances: DSSInstancesResource,
     ):
-        super().__init__()
+        super().__init__(dss_instances.dss_instances)
