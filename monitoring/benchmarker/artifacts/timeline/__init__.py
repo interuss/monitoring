ae789a379589a0ddf196631db2c4c308375b47d6
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2026-08-26 09:14:04 -0700

    [benchmarker] Add timeline artifact (#1664)

---
+++ monitoring/benchmarker/artifacts/timeline/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+jinja_env = Environment(
+    loader=FileSystemLoader(
+        [
+            os.path.abspath(os.path.join(os.path.dirname(__file__), relpath))
+            for relpath in ("templates", "../../../monitorlib/html/templates")
+        ]
+    ),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
