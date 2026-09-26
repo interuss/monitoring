affeb4ea37b7727a9d943922d0846bf017daeb85
Benjamin Pelletier <pelletierb@wing.com>
2026-09-26 02:48:22 +0000

    Add configuration flattening and validation options

--- monitoring/benchmarker/benchmark.py
+++ monitoring/benchmarker/benchmark.py
@@ -1,9 +1,11 @@
 #!env/bin/python3
 
 import argparse
+import json
 import os
 import sys
 
+import yaml
 from loguru import logger
 
 from monitoring.benchmarker.artifacts.generation import (
@@ -21,6 +23,18 @@ def parseArgs() -> argparse.Namespace:
         "--config",
         help="Configuration string indicating file reference (e.g. file://path/to/config.jsonnet)",
         required=True,
+    )
+
+    parser.add_argument(
+        "--config-output",
+        default=None,
+        help="If specified, write the configuration as parsed (potentially from multiple files) to the single file specified by this path",
+    )
+
+    parser.add_argument(
+        "--exit-before-execution",
+        action="store_true",
+        help="If specified, only exit before benchmark execution begins.",
     )
 
     parser.add_argument(
@@ -43,10 +57,29 @@ def parseArgs() -> argparse.Namespace:
 
 def run_config(
     config_name: str,
+    config_output: str | None,
     skip_validation: bool,
+    exit_before_execution: bool,
     output_dir: str,
 ) -> int:
     config = load_config(config_name, skip_validation)
+
+    if config_output:
+        logger.info("Writing flattened configuration to {}", config_output)
+        if config_output.lower().endswith(".json"):
+            with open(config_output, "w") as f:
+                json.dump(config, f, indent=2, sort_keys=True)
+        elif config_output.lower().endswith(".yaml"):
+            with open(config_output, "w") as f:
+                yaml.dump(json.loads(json.dumps(config)), f, sort_keys=True)
+        else:
+            raise ValueError(
+                "Unsupported extension for --config-output; only .json or .yaml file paths may be specified"
+            )
+
+    if exit_before_execution:
+        logger.info("Exiting because --exit-before-execution specified.")
+        return os.EX_OK
 
     run_report = run_benchmark(config, output_dir)
 
@@ -70,7 +103,9 @@ def main() -> int:
     )
     exit_code = run_config(
         args.config,
+        args.config_output,
         args.skip_validation,
+        args.exit_before_execution,
         output_dir,
     )
     if exit_code != os.EX_OK:
